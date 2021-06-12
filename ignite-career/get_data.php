5c3a57c506a8c4b1fd92c621134b4f3992212199
riomadara14 <81938391+riomadara14@users.noreply.github.com>
2021-06-12 17:14:11 +0800

    Ignite Career Sample

---
+++ ignite-career/get_data.php
@@ -0,0 +1,82 @@
+<?php
+
+if(isset($_POST["getData"])){
+    $action = $_POST["getData"];
+
+        Api($action);
+        
+    }
+
+
+function Api($action){
+
+    $url = "https://ignite-careers.com/test/endpoint.php";
+    $data = '{"jobId":10}';
+
+    $headers = array(
+        "Authorization: Bearer qwertyuiop",
+        "Content-Type: application/json",
+    );
+
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+    $response = curl_exec($curl);
+    curl_close($curl);
+    $dex = json_decode($response, true);
+
+    if($action == "get_data"){
+        foreach($dex["data"] as $d){
+
+            echo "<tr>";
+    
+                echo "<td>".$d["uid"]."</td>";
+                echo "<td>".$d["first_name"]."</td>";
+                echo "<td>".$d["last_name"]."</td>";
+                echo "<td>".$d["mobile"]."</td>";
+                echo "<td>".$d["email"]."</td>";
+    
+        echo "</tr>";
+        
+        }
+
+    }
+
+    if($action == "get_download"){
+        $filename ="data.csv";
+        header("Content-Description: File Transfer");
+        header('Content-Type: application/csv');
+        header('Content-Disposition: attachment; filename="'.$filename.'";');
+        ob_end_clean();
+        $csv = fopen('php://output', 'w');
+        fputcsv($csv, array_keys($dex["data"][0]));
+        foreach($dex["data"] as $dat){
+       
+           fputcsv($csv,$dat);
+        }
+        fclose($csv);
+    
+        exit();
+    }
+    
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ ignite-career/ignite-career.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ignited Careers Data Sample</title>
+    <script
+    src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+    crossorigin="anonymous">  
+    </script>
+</head>
+<body>
+
+<table border="1">
+<thead>
+ <tr>
+    <th>Uid</th>
+    <th>First Name</th>
+    <th>Lastname</th>
+    <th>Mobile</th>
+    <th>Email</th>
+  </tr>
+</thead>
+<tbody id="display_data">
+ 
+</tbody>
+
+</table>
+<form action="get_data.php" method="POST">
+    <input type="hidden" value="get_download" name="getData">
+    <button type="submit">Download CSV</button>
+</form>
+  
+    
+</body>
+
+<script>
+
+$.ajax({
+
+    url:'get_data.php',
+    type:'POST',
+    data: {getData:"get_data"},
+
+    success:function(data){
+
+        $("#display_data").html(data); 
+
+    }
+});
+
+
+</script>
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
