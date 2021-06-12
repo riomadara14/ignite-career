a4ba5af6446c8a6bb12cdf0201e337a849c9efd5
riomadara14 <81938391+riomadara14@users.noreply.github.com>
2021-06-12 17:26:14 +0800

    API get Data

--- ignite-career/get_data.php
+++ ignite-career/get_data.php
@@ -18,6 +18,7 @@ function Api($action){
         "Content-Type: application/json",
     );
 
+    
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_POST, true);
